88e37c6bbce75542581a6dafa8aa27cc1a6d9f2e
PanelSerwerson <111978684+PanelSerwerson@users.noreply.github.com>
2025-08-05 01:23:27 +0200

    Update serverbound_hello_packet.rs

--- azalea-protocol/src/packets/login/serverbound_hello_packet.rs
+++ azalea-protocol/src/packets/login/serverbound_hello_packet.rs
@@ -5,7 +5,7 @@ use uuid::Uuid;
 #[derive(Clone, Debug, PartialEq, Eq, McBuf, ServerboundLoginPacket)]
 pub struct ServerboundHelloPacket {
     pub name: String,
-    pub profile_id: Uuid,
+    pub profile_id: Option<Uuid>,
 }
 
 #[cfg(test)]
