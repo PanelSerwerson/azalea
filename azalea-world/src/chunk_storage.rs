7a5e1f2ee47ce3fe4a0a0eff8cd634bc76221531
PanelSerwerson <111978684+PanelSerwerson@users.noreply.github.com>
2025-08-04 15:09:12 +0200

    Update chunk_storage.rs

--- azalea-world/src/chunk_storage.rs
+++ azalea-world/src/chunk_storage.rs
@@ -132,8 +132,7 @@ impl PartialChunkStorage {
     }
 
     pub fn index_from_chunk_pos(&self, chunk_pos: &ChunkPos) -> usize {
-        let view_range = self.view_range as i32;
-
+        let view_range = u32::max(self.view_range, 1) as i32;
         let x = i32::rem_euclid(chunk_pos.x, view_range) * view_range;
         let z = i32::rem_euclid(chunk_pos.z, view_range);
         (x + z) as usize
