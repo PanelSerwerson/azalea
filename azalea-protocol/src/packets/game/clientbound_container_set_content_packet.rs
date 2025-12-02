05efdf5165ca0bd73299c8ba42051e46daae132c
PanelSerwerson <111978684+PanelSerwerson@users.noreply.github.com>
2025-12-02 17:31:34 +0100

    Update clientbound_container_set_content_packet.rs

--- azalea-protocol/src/packets/game/clientbound_container_set_content_packet.rs
+++ azalea-protocol/src/packets/game/clientbound_container_set_content_packet.rs
@@ -1,4 +1,3 @@
-/*
 use azalea_buf::McBuf;
 use azalea_inventory::ItemSlot;
 use azalea_protocol_macros::ClientboundGamePacket;
@@ -10,42 +9,4 @@ pub struct ClientboundContainerSetContentPacket {
     pub state_id: u32,
     pub items: Vec<ItemSlot>,
     pub carried_item: ItemSlot,
-}
-*/
-
-use azalea_buf::{McBufReadable, BufReadExt};
-use azalea_inventory::ItemSlot;
-use azalea_protocol_macros::ClientboundGamePacket;
-use std::io::{self, Cursor};
-use azalea_core::read::try_read_content;
-#[derive(Clone, Debug, ClientboundGamePacket)]
-pub struct ClientboundContainerSetContentPacket {
-    pub container_id: i8,
-    pub state_id: u32, 
-    pub items: Vec<ItemSlot>,
-    pub carried_item: ItemSlot,
-}
-impl McBufReadable for ClientboundContainerSetContentPacket {
-    fn read_from(buf: &mut Cursor<&[u8]>) -> Result<Self, io::Error> {
-        let container_id = i8::read_from(buf)?;
-        let state_id = buf.read_var_u32()?;
-        let items = match try_read_content::<Vec<ItemSlot>>(buf) {
-            Ok(i) => i,
-            Err(e) => {
-                eprintln!(
-                    eprintln!("[PATCH] ContainerSet error, ignoring content in GUI: {}", e);
-                    state_id, e
-                );
-                Vec::new() 
-            }
-        };
-        let carried_item = ItemSlot::read_from(buf)?;
-
-        Ok(ClientboundContainerSetContentPacket {
-            container_id,
-            state_id,
-            items,
-            carried_item,
-        })
-    }
 }
